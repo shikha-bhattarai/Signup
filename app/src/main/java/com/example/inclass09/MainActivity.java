de46b33019dfa3429d7ade93d1ade9871f6492f5
Shikha <sbhatt10@uncc.edu>
2019-04-01 19:35:05 -0400

    Add sign up layout

--- app/src/main/java/com/example/inclass09/MainActivity.java
+++ app/src/main/java/com/example/inclass09/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        emailTextview = (EditText) findViewById(R.id.emailTextview);
-        passTextview = (EditText) findViewById(R.id.passTextview);
+        emailTextview = (EditText) findViewById(R.id.emailEditText);
+        passTextview = (EditText) findViewById(R.id.passEditText);
     }
 }
