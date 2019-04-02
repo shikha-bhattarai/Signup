50d38a06529ea7cb9946bd4564d436b366c4fb05
Teena Xiong <teenaxiong@gmail.com>
2019-04-01 20:13:58 -0400

    working on signup class

--- app/src/main/java/com/example/inclass09/MainActivity.java
+++ app/src/main/java/com/example/inclass09/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.inclass09;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     EditText emailTextview;
     EditText passTextview;
 
@@ -12,7 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        emailTextview = (EditText) findViewById(R.id.emailEditText);
-        passTextview = (EditText) findViewById(R.id.passEditText);
+      emailTextview = (EditText) findViewById(R.id.emailEditText);
+       passTextview = (EditText) findViewById(R.id.passEditText);
+
+        findViewById(R.id.signupbtn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.signupbtn:
+                startActivity(new Intent(this, SignUp.class));
+                break;
+
+        }
     }
 }
